0b2dcae2a3929b535e3d89964c8135abe2d2edb7
kaushaln1 <54100570+kaushaln1@users.noreply.github.com>
2021-05-07 20:48:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,38 @@
 var axios = require('axios');
-const cron = require('node-cron');
 const express = require('express');
 const fs = require('fs');
 app = express();
 let alert = require('alert');  
+const got = require ('got');
 
 //############ INPUTS #################################################### 
 let desriredPincode  = [ 411027, 411038,411011, 412115 ,411001];
-let date = "08-05-2021" ; //dd-mm-yyyy
+let date = "07-05-2021" ; //dd-mm-yyyy
 let interval = 5000;   //5 sec => 5000 , for 1sec => 1000 , 2sec => 2000  
 //##############################################################################
-
 
 
 let responseArray= [];
 async function getData(){
 
    
-    var config = {
-        method: 'get',
-        url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=363&date=${date}`
-      };
-      
-      axios(config)
+  var config = {
+    method: 'get',
+    url: 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=363&date=07-05-2021',
+   
+  };
+      got(config)
       .then(function (response) 
-      { 
-         dataArray = response.data["centers"];
+      {  
+
+        response = JSON.parse(response.body);
+         dataArray = response["centers"];
          for(data in dataArray){
              let sessionData= dataArray[data].sessions;
+             
+
              if (desriredPincode.includes(dataArray[data].pincode ) &&  sessionData[0].min_age_limit!= 45 )
-                {
+                {      
                         responseArray.push({ name: dataArray[data].name , pincode: dataArray[data].pincode , availaible: sessionData[0].available_capacity}  );
                         if ( sessionData[0].available_capacity !=0 ){
                             // write to file 
@@ -46,7 +49,7 @@ async function getData(){
 
          }
       
-         console.log(responseArray);
+        
       })
       .catch(function (error) {
         console.log(error);
